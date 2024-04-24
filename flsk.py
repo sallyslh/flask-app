ddc9b8960f700e8e8a60c60c7d1e0bb3a88f444e
sallyslh <159760400+sallyslh@users.noreply.github.com>
2024-04-24 18:05:53 +0300

    flsk.py

--- flsk.py
+++ flsk.py
@@ -18,8 +18,6 @@ def index():
         subject = request.form.get('subject')
         
         options = webdriver.ChromeOptions()
-        options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
 
